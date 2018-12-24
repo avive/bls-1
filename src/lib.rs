2d8b7010b006bcad8fdf6cf18eb2a26b171e5e54
Aviv Eyal <aviveyal07@gmail.com>
2018-12-24 17:28:35 +0200

    Fix build and tests

--- src/lib.rs
+++ src/lib.rs
@@ -5,8 +5,7 @@ use pairing::{CurveAffine, CurveProjective, Engine, Field};
 use rand::{Rand, Rng};
 use std::collections::HashSet;
 
-const HASH_KEY: &[u8] = b"BLSSignatureSeed";
-
+#[derive(Debug, PartialEq)]
 pub struct Signature<E: Engine> {
     s: E::G1,
 }
@@ -23,13 +22,21 @@ impl<E: Engine> SecretKey<E> {
     }
 
     pub fn sign(&self, message: &[u8]) -> Signature<E> {
-        let h = E::G1Affine::hash(HASH_KEY, message);
+        let h = E::G1Affine::hash(message);
         Signature { s: h.mul(self.x) }
     }
 }
 
 pub struct PublicKey<E: Engine> {
     p_pub: E::G2,
+}
+
+impl<E: Engine> Clone for PublicKey<E> {
+    fn clone(&self) -> Self {
+        Self {
+            p_pub: self.p_pub.clone()
+        }
+    }
 }
 
 impl<E: Engine> PublicKey<E> {
@@ -41,7 +48,7 @@ impl<E: Engine> PublicKey<E> {
     }
 
     pub fn verify(&self, message: &[u8], signature: &Signature<E>) -> bool {
-        let h = E::G1Affine::hash(HASH_KEY, message);
+        let h = E::G1Affine::hash(message);
         let lhs = E::pairing(signature.s, E::G2Affine::one());
         let rhs = E::pairing(h, self.p_pub);
         lhs == rhs
@@ -69,6 +76,7 @@ impl<E: Engine> Keypair<E> {
     }
 }
 
+#[derive(Debug, PartialEq)]
 pub struct AggregateSignature<E: Engine>(Signature<E>);
 
 impl<E: Engine> AggregateSignature<E> {
@@ -98,8 +106,30 @@ impl<E: Engine> AggregateSignature<E> {
         let lhs = E::pairing(self.0.s, E::G2Affine::one());
         let mut rhs = E::Fqk::one();
         for input in inputs {
-            let h = E::G1Affine::hash(HASH_KEY, input.1);
+            let h = E::G1Affine::hash(input.1);
             rhs.mul_assign(&E::pairing(h, input.0.p_pub));
+        }
+        lhs == rhs
+    }
+
+    /// Verify a aggregate signatures over a common message.
+    ///
+    /// Warning: This method is vulnerable to the "rouge public-key attack".
+    /// Every user must be required to prove knowledge or possession of their
+    /// corresponding secret key. For more information, see:
+    /// https://eprint.iacr.org/2018/483.pdf
+    pub fn verify_common_message(
+        &self,
+        message: &[u8],
+        pubkeys: &Vec<&PublicKey<E>>)
+        -> bool
+    {
+        let h = E::G1Affine::hash(message);
+        // Check pairings
+        let lhs = E::pairing(self.0.s, E::G2Affine::one());
+        let mut rhs = E::Fqk::one();
+        for pubkey in pubkeys {
+            rhs.mul_assign(&E::pairing(h, pubkey.p_pub));
         }
         lhs == rhs
     }
@@ -113,32 +143,134 @@ mod tests {
     use rand::{SeedableRng, XorShiftRng};
 
     #[test]
-    fn sign_verify() {
+    fn test_sign_verify_short() {
+        sign_verify(10);
+    }
+    #[test]
+    #[ignore]
+    fn test_sign_verify_long() {
+        sign_verify(500);
+    }
+    fn sign_verify(loop_count: u32) {
         let mut rng = XorShiftRng::from_seed([0xbc4f6d44, 0xd62f276c, 0xb963afd0, 0x5455863d]);
 
-        for i in 0..500 {
+        for i in 0..loop_count {
             let keypair = Keypair::<Bls12>::generate(&mut rng);
-            let message = format!("Message {}", i);
+            let message = format!(">16 character message {}", i);
             let sig = keypair.sign(&message.as_bytes());
             assert_eq!(keypair.verify(&message.as_bytes(), &sig), true);
         }
     }
 
     #[test]
-    fn aggregate_signatures() {
+    fn test_sign_verify_with_clone_pubic_short() {
+        sign_verify_with_cloned_public(10);
+    }
+    #[test]
+    #[ignore]
+    fn test_sign_verify_with_clone_pubic_long() {
+        sign_verify_with_cloned_public(500);
+    }
+    fn sign_verify_with_cloned_public(loop_count: u32) {
+        let mut rng = XorShiftRng::from_seed([0xbc4f6d44, 0xd62f276c, 0xb963afd0, 0x5455863d]);
+
+        for i in 0..loop_count {
+            let keypair = Keypair::<Bls12>::generate(&mut rng);
+            let message = format!(">16 character message {}", i);
+            let sig = keypair.sign(&message.as_bytes());
+            let cloned_pub = keypair.public.clone();
+            assert_eq!(cloned_pub.verify(&message.as_bytes(), &sig), true);
+        }
+    }
+
+    #[test]
+    fn test_sign_verify_aggregate_common_message_short() {
+        sign_verify_aggregate_common_message(10);
+    }
+    #[test]
+    #[ignore]
+    fn test_sign_verify_aggregate_common_message_long() {
+        sign_verify_aggregate_common_message(500);
+    }
+    fn sign_verify_aggregate_common_message(loop_count: u32) {
+        let mut rng = XorShiftRng::from_seed([0xbc4f6d44, 0xd62f276c, 0xb963afd0, 0x5455863d]);
+
+        let mut pubkeys = Vec::with_capacity(1000);
+        let mut signatures = Vec::with_capacity(1000);
+        let message = ">16 character indentical message".as_bytes();
+        for i in 0..loop_count {
+            let keypair = Keypair::<Bls12>::generate(&mut rng);
+            let signature = keypair.sign(&message);
+            pubkeys.push(keypair.public);
+            signatures.push(signature);
+
+            if i < 10 || i > (loop_count - 5) {
+                let asig = AggregateSignature::from_signatures(&signatures);
+                assert_eq!(
+                    asig.verify_common_message(&message, &pubkeys.iter().map(|&ref pk| pk).collect()),
+                    true
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn test_sign_verify_aggregate_common_message_missing_sig_short() {
+        sign_verify_aggregate_common_message_missing_sig(10);
+    }
+    #[test]
+    #[ignore]
+    fn test_sign_verify_aggregate_common_message_missing_sig_long() {
+        sign_verify_aggregate_common_message_missing_sig(500);
+    }
+    fn sign_verify_aggregate_common_message_missing_sig(loop_count: u32) {
+        let mut rng = XorShiftRng::from_seed([0xbc4f6d44, 0xd62f276c, 0xb963afd0, 0x5455863d]);
+
+        let skipped_sig = loop_count / 2;
+        let mut pubkeys = Vec::with_capacity(1000);
+        let mut signatures = Vec::with_capacity(1000);
+        let message = ">16 character indentical message".as_bytes();
+        for i in 0..loop_count {
+            let keypair = Keypair::<Bls12>::generate(&mut rng);
+            let signature = keypair.sign(&message);
+            pubkeys.push(keypair.public);
+            if i != skipped_sig {
+                signatures.push(signature);
+            }
+
+            if i < 10 || i > (loop_count - 5) {
+                let asig = AggregateSignature::from_signatures(&signatures);
+                assert_eq!(
+                    asig.verify_common_message(&message, &pubkeys.iter().map(|&ref pk| pk).collect()),
+                    i < skipped_sig
+                );
+            }
+        }
+    }
+
+    #[test]
+    fn aggregate_signatures_short() {
+        aggregate_signatures(20);
+    }
+    #[test]
+    #[ignore]
+    fn aggregate_signatures_long() {
+        aggregate_signatures(500);
+    }
+    fn aggregate_signatures(loop_count: u32) {
         let mut rng = XorShiftRng::from_seed([0xbc4f6d44, 0xd62f276c, 0xb963afd0, 0x5455863d]);
 
         let mut inputs = Vec::with_capacity(1000);
         let mut signatures = Vec::with_capacity(1000);
-        for i in 0..500 {
+        for i in 0..loop_count {
             let keypair = Keypair::<Bls12>::generate(&mut rng);
-            let message = format!("Message {}", i);
+            let message = format!(">16 character message {}", i);
             let signature = keypair.sign(&message.as_bytes());
             inputs.push((keypair.public, message));
             signatures.push(signature);
 
             // Only test near the beginning and the end, to reduce test runtime
-            if i < 10 || i > 495 {
+            if i < 10 || i > (loop_count - 5) {
                 let asig = AggregateSignature::from_signatures(&signatures);
                 assert_eq!(
                     asig.verify(&inputs
@@ -160,7 +292,7 @@ mod tests {
 
         // Create the first signature
         let keypair = Keypair::<Bls12>::generate(&mut rng);
-        let message = "First message";
+        let message = ">16 char first message";
         let signature = keypair.sign(&message.as_bytes());
         inputs.push((keypair.public, message));
         asig.aggregate(&signature);
@@ -176,7 +308,7 @@ mod tests {
 
         // Create the second signature
         let keypair = Keypair::<Bls12>::generate(&mut rng);
-        let message = "Second message";
+        let message = ">16 char second message";
         let signature = keypair.sign(&message.as_bytes());
         inputs.push((keypair.public, message));
         asig.aggregate(&signature);
